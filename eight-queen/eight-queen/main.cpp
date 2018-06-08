6c50a15eff802b4aeb2cfca9a5122dc97ad90ead
huxinyan <anonymous@torzo.club>
2018-06-08 19:59:03 +0800

    创建项目

---
+++ eight-queen/eight-queen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eight-queen
+//
+//  Created by 胡欣妍 on 2018/6/8.
+//  Copyright © 2018年 胡欣妍. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
